0b173bc74c520500ce79ad08df9bcff84b9e6c47
HarshadKawale <harshad.kawale@gmail.com>
2025-04-10 21:19:08 +0000

    second commit

--- funtional-programming/src/App.java
+++ funtional-programming/src/App.java
@@ -1,5 +1,28 @@
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+printAllNumbersInListStructured(List.of(10,12,13,14,15,16,17,18,19,20));
+printAllNumbersInListFunctional(List.of(10,12,13,14,15,16,17,18,19,20));
+}
+
+    private static void printNumbers(int of) {
+        
+        System.out.println(of);
+    }
+
+    private static void printAllNumbersInListFunctional(List<Integer> of) {
+            of.stream().forEach(App::printNumbers);
+    }
+
+    private static void printAllNumbersInListStructured(List<Integer> of) {
+       // for(int numbers : of){
+
+            //System.out.println( numbers);
+
+        //}
+
+
     }
 }
+
